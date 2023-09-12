c5093a41cef3f52a6ecd09ee5b7046a65da390bf
Alexy Nefedov <caleo79111@gmail.com>
2023-09-12 20:11:51 +0300

    task56

---
+++ task56/Program.cs
@@ -0,0 +1,82 @@
+﻿/*
+Задача 56: Задайте прямоугольный двумерный массив. 
+Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Программа считает сумму элементов в каждой строке 
+и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+*/
+// Ввод в консоль                                 
+int InputNum (string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+//Создание двумерного массива
+int[,] Create2DArray(int rows, int columns)
+{
+    return new int[rows, columns];
+}
+//Заполнение массива рандомными числами
+void Fill2DArray(int[,] array)
+{
+    Random random = new Random();
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.Next(10, 100);
+        }  
+    }
+}
+//Выводим в консоль массив
+void Print2DArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} \t");
+        }
+        Console.WriteLine();  
+    }  
+}
+//Вывод номера строки с наименьшей суммой элементов 
+void NumberRowMinSumElements(int[,] array)
+{
+    int minRow = 0;
+    int sumRow = 0;
+    int minSumRow = 0;
+    
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        minRow += array[0, i];
+    }
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++) 
+        {
+            sumRow += array[i, j];
+        }
+        if (sumRow < minRow)
+        {
+            minRow = sumRow;
+            minSumRow = i;
+        }
+        sumRow = 0;
+    }
+    Console.WriteLine($"{minSumRow + 1} строка");
+}
+//Ввод данных в консоль
+int rows = InputNum("Введите количество строк: ");
+int columns = InputNum("Введите количество столбцов: ");
+//Вывод
+int[,] myArray = Create2DArray(rows, columns);
+Fill2DArray(myArray);
+Print2DArray(myArray);
+Console.WriteLine();
+NumberRowMinSumElements(myArray);
